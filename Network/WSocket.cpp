49fa44463dbf1f7de7e135588a80a73e199e5468
jyb <jybwarrior@gmail.com>
2014-04-30 11:53:02 +0200

    WSA Startup ( Socket Init for windows )

--- Network/WSocket.cpp
+++ Network/WSocket.cpp
@@ -1,10 +1,23 @@
 #ifdef _WIN32
 
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include "WSocket.hh"
+
+#pragma comment(lib, "ws2_32.lib")
 
 void Socket::init()
 {
+	WORD wVersionRequested;
+
+	wVersionRequested = MAKEWORD(2, 2);
+	int res = WSAStartup(wVersionRequested, &(this->wsaData));
+	if (res != 0)
+	{
+		// Class d'exception a faire
+		std::cerr << "WSAStartup failed: " << res << std::endl;
+	}
 	std::cout << "win init" << std::endl;
 }
 
--- Network/WSocket.hh
+++ Network/WSocket.hh
@@ -1,10 +1,15 @@
 #ifndef		WSOCKET_H_
 # define	WSOCKET_H_
 
+#include <windows.h>
+#include <winsock.h>
 #include "ISocket.hh"
 
 class Socket : public ISocket
 {
+protected :
+	WSADATA wsaData;
+
 public :
   void init();
   void recieve();
