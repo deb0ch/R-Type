e18a5460286d77cd84aa9709639837b22ea6d8a5
jyb <jybwarrior@gmail.com>
2014-05-03 11:14:05 +0200

    correction of a bug in ExcpetionNetwork

--- Network/NetworkException.cpp
+++ Network/NetworkException.cpp
@@ -1,28 +1,44 @@
 #include <sstream>
+#include <cstring>
 #include "NetworkException.hh"
 
 std::map<NetworkException::ERROR_TYPE, const std::string> NetworkException::_msg = {
-  {NetworkException::NONE, "Exception : "},
-  {NetworkException::TCP, "Socket TCP Exception : "},
-  {NetworkException::UDP, "Socket UDP Exception : "}
+	{ NetworkException::NONE, "Exception : " },
+	{ NetworkException::TCP, "Socket TCP Exception : " },
+	{ NetworkException::UDP, "Socket UDP Exception : " }
 };
 
-NetworkException::NetworkException(const NetworkException::ERROR_TYPE type, const int error) {
-  if (NetworkException::_msg.find(type) != NetworkException::_msg.end())
-    this->_info = NetworkException::_msg[type];
-  else
-    this->_info = NetworkException::_msg[NONE];
-  this->_info += strerror(error);
+void NetworkException::addError(const int error)
+{
+	char buffer[64];
+	std::string res;
+
+#ifdef _WIN32
+	strerror_s(buffer, sizeof(buffer), error);
+	this->_info += buffer;
+#elif __linux__
+	strerror_r(error, buffer, sizeof(buffer));
+	this->_info += buffer;
+#endif
+}
+
+NetworkException::NetworkException(const NetworkException::ERROR_TYPE type, const int error)
+{
+	if (NetworkException::_msg.find(type) != NetworkException::_msg.end())
+		this->_info = NetworkException::_msg[type];
+	else
+		this->_info = NetworkException::_msg[NONE];
+	this->addError(error);
 }
 
 NetworkException::NetworkException(const NetworkException::ERROR_TYPE type, const std::string &info) {
-  if (NetworkException::_msg.find(type) != NetworkException::_msg.end())
-    this->_info = NetworkException::_msg[type];
-  else
-    this->_info = NetworkException::_msg[NONE];
-  this->_info += info;
+	if (NetworkException::_msg.find(type) != NetworkException::_msg.end())
+		this->_info = NetworkException::_msg[type];
+	else
+		this->_info = NetworkException::_msg[NONE];
+	this->_info += info;
 }
 
 const char*   NetworkException::what() const throw() {
-  return (this->_info.c_str());
+	return (this->_info.c_str());
 }
--- Network/NetworkException.hh
+++ Network/NetworkException.hh
@@ -14,24 +14,25 @@
 class NetworkException : public std::exception
 {
 public:
-  enum	ERROR_TYPE {
-    NONE,
-    SELECT,
-    UDP,
-    TCP
-  };
+	enum	ERROR_TYPE {
+		NONE,
+		SELECT,
+		UDP,
+		TCP
+	};
 
-  static std::map<NetworkException::ERROR_TYPE, const std::string> _msg;
+	static std::map<NetworkException::ERROR_TYPE, const std::string> _msg;
 
 public:
-  NetworkException(const NetworkException::ERROR_TYPE type, const int error);
-  NetworkException(const NetworkException::ERROR_TYPE type, const std::string &error);
-  ~NetworkException() throw() {}
-  const char* what() const throw();
+	NetworkException(const NetworkException::ERROR_TYPE type, const int error);
+	NetworkException(const NetworkException::ERROR_TYPE type, const std::string &error);
+	~NetworkException() throw() {}
+	const char* what() const throw();
 
 private:
-  std::string	_info;
-  ERROR_TYPE	_type;
+	void addError(const int error);
+	std::string	_info;
+	ERROR_TYPE	_type;
 
 };
 
