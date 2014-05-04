4ab0271e0b1a007a238cb3325a1e448841318f9c
yalap_j <yalap_j@epitech.eu>
2014-05-04 18:41:17 +0200

    ajout des Image loader avec rectangle

--- utility/ImageLoader.cpp
+++ utility/ImageLoader.cpp
@@ -1,34 +1,56 @@
 #include "ImageLoader.hh"
 
 ImageLoader::ImageLoader() {
-  this->_images = std::map<const std::string, sf::Texture *>();
+  this->_images = std::map<std::string, std::pair<sf::Texture *, ImageLoader::InfImg> >();
 }
 
 ImageLoader::~ImageLoader() {
   auto it = this->_images.begin();
 
   for (it = this->_images.begin(); it != this->_images.end(); ++it) {
-    delete it->second;
+    delete it->second.first;
   }
 }
 
-void ImageLoader::addImage(const std::string &fileImage) {
+void ImageLoader::addImage(const std::string &fileImage, ImageLoader::NbSprite nbSprite) {
   auto it = this->_images.find(fileImage);
 
   if (it == this->_images.end()) {
     sf::Texture	*image = new sf::Texture();
 
     if (!image->loadFromFile(fileImage))
-      return; //TODO throw;
-    this->_images[fileImage] = image;
+      throw 4; //TODO throw;
+    if (nbSprite.nbSprintX == 0 || nbSprite.nbSprintY == 0)
+      throw 5; //TODO throw
+    sf::Vector2u v(image->getSize().x / nbSprite.nbSprintX, image->getSize().y / nbSprite.nbSprintY);
+    ImageLoader::InfImg infoImg;
+    infoImg.nbSprite = nbSprite;
+    infoImg.rect = v;
+    this->_images[fileImage] = std::pair<sf::Texture *, ImageLoader::InfImg>(image, infoImg);
   }
 }
 
-sf::Texture *ImageLoader::getImage(const std::string &fileImage) const {
+const std::pair<sf::Texture *, ImageLoader::InfImg> &ImageLoader::getPair(const std::string
+									  &fileImage) const
+{
+
   auto it = this->_images.find(fileImage);
 
   if (it != this->_images.end()) {
-    return (it->second);
+    return ((it->second));
   }
-  return NULL;
+  throw 5;  //TODO throw
+}
+
+sf::Sprite *ImageLoader::createSprite(const std::string &fileImage,
+				      const unsigned int  numSprite) const {
+  sf::Sprite *sprite = new sf::Sprite();
+  //TODO try catch
+  auto _pair = this->getPair(fileImage);
+
+  sprite->setTexture(*_pair.first);
+  int x = (numSprite % _pair.second.nbSprite.nbSprintX) * _pair.second.rect.x;
+  int y = (numSprite / _pair.second.nbSprite.nbSprintX) * _pair.second.rect.y;
+  sprite->setTextureRect(sf::IntRect(x, y, _pair.second.rect.x, _pair.second.rect.y));
+  return (sprite);
 }
--- utility/ImageLoader.hh
+++ utility/ImageLoader.hh
@@ -7,15 +7,32 @@
 class ImageLoader
 {
 public:
+
+  typedef struct	s_NbSprite
+  {
+    unsigned int	nbSprintX;
+    unsigned int	nbSprintY;
+  }			NbSprite;
+
+  typedef struct	s_InfImg
+  {
+    NbSprite		nbSprite;
+    sf::Vector2u	rect;
+  }			InfImg;
+
+public:
   ImageLoader();
   virtual ~ImageLoader();
 
 public:
-  void addImage(const std::string &fileImage);
-  sf::Texture *getImage(const std::string &fileImage) const;
+  void addImage(const std::string &fileImage, ImageLoader::NbSprite nbSprite);
+  sf::Sprite *createSprite(const std::string &fileImage, const unsigned int numSprite) const;
 
 private:
-  std::map<const std::string, sf::Texture *> _images;
+  const std::pair<sf::Texture *, ImageLoader::InfImg> &getPair(const std::string &fileImage) const;
+
+private:
+  std::map<std::string, std::pair<sf::Texture *, ImageLoader::InfImg> > _images;
 
 private:
   ImageLoader(const ImageLoader &);
