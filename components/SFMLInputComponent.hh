92ff6cef454f4cc197d1cb37ea0c8769160c38b6
demess_p <demess_p@epitech.eu>
2014-05-05 14:55:50 +0200

    Updated the SFMLInputSystem logic so it checks for SFML::Keyboard once a tick and not for each entity.

--- components/SFMLInputComponent.hh
+++ components/SFMLInputComponent.hh
@@ -1,24 +1,24 @@
 #ifndef		SFMLINPUTCOMPONENT_H_
 # define	SFMLINPUTCOMPONENT_H_
 
-#include <map>
-#include "SFML/Window/Keyboard.hpp"
-#include "SFML/Window/Event.hpp"
-#include "AComponent.hh"
+#include	<map>
 
-class		SFMLInputComponent : public AComponent
+#include	"SFML/Window/Keyboard.hpp"
+#include	"SFML/Window/Event.hpp"
+
+#include	"AComponent.hpp"
+
+class		SFMLInputComponent : public AComponent<SFMLInputComponent>
 {
+private:
+  std::map<sf::Keyboard::Key, bool>	_inputs;
+
 public:
   SFMLInputComponent();
   virtual ~SFMLInputComponent();
-  const std::map<sf::Keyboard::Key, bool> getInputs() const;
-  bool isActived(const sf::Keyboard::Key key) const;
-  void setStatusKey(const sf::Keyboard::Key key, const bool status);
-
-private:
-  std::map<sf::Keyboard::Key, bool> _inputs;
-
+  const std::map<sf::Keyboard::Key, bool>	getInputs() const;
+  bool						isActived(const sf::Keyboard::Key key) const;
+  void						setStatusKey(const sf::Keyboard::Key key, const bool status);
 };
-
 
 #endif /* !SFMLINPUTCOMPONENT_H_ */
--- systems/SFMLInputSystem.hh
+++ systems/SFMLInputSystem.hh
@@ -1,19 +1,22 @@
-#ifndef SFMLINPUTSYSTEM_H_
-# define SFMLINPUTSYSTEM_H_
+#ifndef		SFMLINPUTSYSTEM_H_
+# define	SFMLINPUTSYSTEM_H_
 
-# include		"ASystem.hh"
+#include	"SFML/Window/Keyboard.hpp"
 
-class			SFMLInputSystem : public ASystem
+# include	"ASystem.hh"
+
+class		SFMLInputSystem : public ASystem
 {
 protected:
-  virtual bool		canProcess(Entity *);
-  virtual void		processEntity(Entity *, const float);
+  std::map<sf::Keyboard::Key, bool>	_inputs;
+
+  virtual void	beforeProcess();
+  virtual bool	canProcess(Entity *);
+  virtual void	processEntity(Entity *, const float);
 
 public:
   SFMLInputSystem();
-  virtual		~SFMLInputSystem();
-
+  virtual	~SFMLInputSystem();
 };
-
 
 #endif /* !SFMLINPUTSYSTEM_H_ */
