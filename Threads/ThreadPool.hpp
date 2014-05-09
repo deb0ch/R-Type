4459fec2455b4804e7a3d40fa6bc8ef6b9f8317b
yalap_j <yalap_j@epitech.eu>
2014-05-09 19:50:49 +0200

    rien de special

--- Threads/ThreadPool.hpp
+++ Threads/ThreadPool.hpp
@@ -8,36 +8,60 @@
 template <typename T>
 class ThreadPool
 {
-private :
-	std::vector<Thread<T> *> pool;
-
-private :
-	ThreadPool() = delete;
-	ThreadPool(const ThreadPool& other) = delete;
-	ThreadPool &operator=(const ThreadPool & other) = delete;
-
 public :
-	ThreadPool(int nb_thread)
-	{
-		for (int i = 0; i < nb_thread; i++)
-		{
-			this->pool.push_back(new Thread<T>());
-		}
-	}
 
-	void AddThreadToPool(Any arg, T* obj, void (T::*fct)(Any &))
-	{
-		this->pool.push_back(new Thread<T>());
-		this->pool.back()->start(arg, obj, fct);
-	}
+  ThreadPool(unsigned int nbThread) {
+    Thread<T> *thread;
+    this->_nbThread = nbThread;
+    for (unsigned int i = 0; i < nbThread; i++) {
+	this->_pool.push_back(new Thread<T>());
+      }
+    this->StartPooling();
+  }
 
-	void StartPooling(Any arg, T* obj, void (T::*fct)(Any &))
-	{
-		for (unsigned int i = 0; i < this->pool.size(); i++)
-		{
-			this->pool[i]->start(arg, obj, fct);
-		}
-	}
+  void StartPooling(Any arg, T* obj, void (T::*fct)(Any &)) {
+    for (unsigned int i = 0; i < this->_pool.size(); i++) {
+	this->_pool[i]->start(i, this, run);
+      }
+  }
+
+  void addTask(Any arg, T* obj, void (T::*fct)(Any &)) {
+    Container *container = new Container();
+    container->arg = arg;
+    container->obj = obj;
+    container->fct = fct;
+    this->_task.push(container);
+    //Envoi broacast a tous les thread via les variable conditionnelle
+  }
+
+  void	run(unsigned int count) {
+    Container *toto;
+
+    while (1) {
+      // cond var wait
+      if ((toto = this->_task.getNext()) != NULL) {
+	this->_pool[count]->start(toto);
+      }
+    }
+  }
+
+private :
+  ThreadPool() = delete;
+  ThreadPool(const ThreadPool& other) = delete;
+  ThreadPool &operator=(const ThreadPool & other) = delete;
+
+private :
+  struct Container
+  {
+    T *obj;
+    void (T::*fct)(Any &);
+    Any arg;
+  };
+
+  SafeFifo<Container *> _task;
+  std::vector<Thread<T> *> _pool;
+  unsigned int _nbThread;
+
 };
 
 #endif /* !THREADPOOL_H_ */
