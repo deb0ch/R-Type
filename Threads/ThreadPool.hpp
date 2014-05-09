d3c0888f51b00e7860b361c9ae50f2573727548d
jyb <jybwarrior@gmail.com>
2014-05-09 11:27:30 +0200

    remove warning from the main test

---
+++ Threads/ThreadPool.hpp
@@ -0,0 +1,37 @@
+#ifndef THREADPOOL_H_
+# define THREADPOOL_H_
+
+#include <vector>
+#include "IThread.hpp"
+#include "Threads.hh"
+
+template <typename T>
+class ThreadPool
+{
+private :
+	std::vector<Thread<T> *> pool;
+
+private :
+	ThreadPool() = delete;
+	ThreadPool(const ThreadPool& other) = delete;
+	ThreadPool &operator=(const ThreadPool & other) = delete;
+
+public :
+	ThreadPool(int nb_thread)
+	{
+		for (int i = 0; i < nb_thread; i++)
+		{
+			this->pool.push_back(new Thread<T>());
+		}
+	}
+
+	void StartPooling(Any arg, T* obj, void (T::*fct)(Any &))
+	{
+		for (unsigned int i = 0; i < this->pool.size(); i++)
+		{
+			this->pool[i]->start(arg, obj, fct);
+		}
+	}
+};
+
+#endif /* !THREADPOOL_H_ */
