1af21a4855a4dd023e1b99bda00b7bb10a450a38
yalap_j <yalap_j@epitech.eu>
2014-05-12 21:09:05 +0200

    ajout de ComponentFactory.hpp

--- ECS/ComponentFactory.hpp
+++ ECS/ComponentFactory.hpp
@@ -2,40 +2,66 @@
 # define COMPONENTFACTORY_H_
 
 # include <map>
+# include "Unistd.hh"
 # include "Factory.hpp"
 # include "IComponent.hh"
-# include ""
+# include "Hash.hh"
 
 #  ifdef _WIN32
 #   include "WLibraryLoader.hpp"
-#   define PATH "Ressources\\Images\\"
-#   define PATHLIB "..\\plugin\\Debug\\"
-#   define EXTENSION ".dll"
+#   include <sys/types.h>
 #  elif __linux__
-#   include <unistd.h>
 #   include "ULibraryLoader.hpp"
-#   define PATH "Ressources/Images/"
-#   define PATHLIB "plugin/lib"
-#   define EXTENSION ".so"
+#   include <dirent.h>
 #  endif
-
 
 class ComponentFactory : public Factory<IComponent, unsigned long>
 {
 public:
   ComponentFactory(){
+#  ifdef _WIN32
+    this->_directory = "..\\plugin\\Debug\\";
+#  elif __linux__
+    this->_directory = "plugin/lib";
+#  endif
+
     this->_loaderlib = LibraryLoader<IComponent>();
   }
+
   virtual ~ComponentFactory(){
   }
 
   virtual void init() {
-    IComponent *input = NULL;
-    input = this->_loaderlib.getInstance(PATHLIB + std::string("NameFile") + EXTENSION, "getInstance");
-    this->add()
+    IComponent *input;
+    DIR* rep = NULL;
+    struct dirent* file = NULL;
+
+    if ((rep = opendir(this->_directory.c_str())) == NULL)
+      ; //TODO throw
+
+    while ((file = readdir(rep)) != NULL)
+      {
+	input = NULL;
+#  ifdef _WIN32
+	if (auto it = std::string(file->d_name).find(".dll"))
+	  input = _loaderlib.getInstance(this->_directory + file->d_name, "getInstance");
+#  elif __linux__
+	if (auto it = std::string(file->d_name).find(".so"))
+	  {
+	    std::cout << this->_directory + file->d_name << std::endl;
+	    input = _loaderlib.getInstance(this->_directory + file->d_name, "getInstance");
+	  }
+#  endif
+	if (input != NULL)
+	  this->add(Hash()(input->getType()), input);
+      }
+    if (closedir(rep) == -1)
+      ; //TODO throw
   }
+
 private:
   LibraryLoader<IComponent> _loaderlib;
+  std::string _directory;
 
 };
 
