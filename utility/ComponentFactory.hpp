f2917fd4f090ea228e60da13ee3f8127fbdef474
jyb <jybwarrior@gmail.com>
2014-05-13 17:09:59 +0200

    implementation of the component factory

--- utility/ComponentFactory.hpp
+++ utility/ComponentFactory.hpp
@@ -2,9 +2,35 @@
 # define COMPONENTFACTORY_H_
 
 # include "Factory.hpp"
+# include "SFMLInputComponent.hh"
+# include "IComponent.hh"
+# include "Hash.hh"
 
-class CompententFactory : public Factory
+class ComponentFactory : public Factory<IComponent, unsigned long>
 {
+public :
+	ComponentFactory()
+	{ }
+
+	virtual ~ComponentFactory(){
+	}
+
+	virtual void init()
+	{
+		/*
+		IComponent *input = new SFMLInputComponent();
+		this->add(Hash()(input->getType()), input);
+
+		input = new Pos2DComponent(0.0f, 100.0f);
+		this->add(Hash()(input->getType()), input);
+
+		input = new Friction2DComponent(0.5f);
+		this->add(Hash()(input->getType()), input);
+
+		input = new Box2DComponent(50.0f, 50.0f);
+		this->add(Hash()(input->getType()), input);
+		*/
+	}
 };
 
 #endif
