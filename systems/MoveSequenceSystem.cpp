96714fd36845e260c97e16d091c06f81f2109f11
demess_p <demess_p@epitech.eu>
2014-05-18 20:07:52 +0200

    Removed useless test of component and fixed indent.

--- systems/MoveSequenceSystem.cpp
+++ systems/MoveSequenceSystem.cpp
@@ -11,26 +11,23 @@ MoveSequenceSystem::~MoveSequenceSystem()
 
 bool MoveSequenceSystem::canProcess(Entity *entity)
 {
-	if (entity->hasComponent("MoveSequenceComponent") &&
-		entity->hasComponent("ActionComponent") &&
-		entity->hasComponent("Pos2DComponent"))
-		return (true);
-	return (false);
+  if (entity->hasComponent("MoveSequenceComponent") &&
+      entity->hasComponent("ActionComponent"))
+    return (true);
+  return (false);
 }
 
 void MoveSequenceSystem::processEntity(Entity *entity, const float)
 {
-	ActionComponent		*action;
-	MoveSequenceComponent *moveEntity;
-	Pos2DComponent		*posEntity;
+  ActionComponent		*action;
+  MoveSequenceComponent *moveEntity;
 
-	action = entity->getComponent<ActionComponent>("ActionComponent");
-	moveEntity = entity->getComponent<MoveSequenceComponent>("MoveSequenceComponent");
-	posEntity = entity->getComponent<Pos2DComponent>("Pos2DComponent");
+  action = entity->getComponent<ActionComponent>("ActionComponent");
+  moveEntity = entity->getComponent<MoveSequenceComponent>("MoveSequenceComponent");
 
-	if (moveEntity != NULL)
-	{
-		action->setAction(moveEntity->getAction(), true);
-		moveEntity->incrementTick();
-	}
+  if (moveEntity != NULL)
+    {
+      action->setAction(moveEntity->getAction(), true);
+      moveEntity->incrementTick();
+    }
 }
