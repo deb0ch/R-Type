d0fa198c4b4612142ad3417d438a2a6345d5f6c4
yalap_j <yalap_j@epitech.eu>
2014-05-18 18:14:49 +0200

    correction de pa parti unix de NetworkException.cpp

--- Network/NetworkException.cpp
+++ Network/NetworkException.cpp
@@ -9,15 +9,13 @@
 void NetworkException::addError(const int error)
 {
   char buffer[512];
-  std::string res;
 
   memset(buffer, 0, 512);
 #ifdef _WIN32
   strerror_s(buffer, sizeof(buffer), error);
   this->_info += buffer;
 #elif __linux__
-  strerror_r(error, buffer, sizeof(buffer));
-  this->_info += buffer;
+  this->_info += strerror_r(error, buffer, sizeof(buffer));
 #endif
 }
 
