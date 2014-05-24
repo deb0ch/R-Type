2c03057ee6c19970f7d469c229043d3e3895818b
jyb <jybwarrior@gmail.com>
2014-05-24 19:20:44 +0200

    it works on Windows

--- ECS/ComponentFactory.hpp
+++ ECS/ComponentFactory.hpp
@@ -1,28 +1,61 @@
 #ifndef COMPONENTFACTORY_H_
 # define COMPONENTFACTORY_H_
 
-# include <map>
+# include <string>
 # include "Factory.hpp"
-# include "IComponent.hh"
+# include "ASerializableComponent.hh"
+# include "SFMLInputComponent.hh"
+# include "Friction2DComponent.hh"
+# include "Box2DComponent.hh"
+# include "Pos2DComponent.hh"
+# include "Speed2DComponent.hh"
+# include "MovementSpeedComponent.hh"
+# include "SFMLSpriteComponent.hh"
+# include "NetworkSendUpdateComponent.hh"
+# include "ActionComponent.hh"
+# include "PlayerMovementComponent.hh"
+# include "NetworkSendActionComponent.hh"
 # include "Hash.hh"
 
-class ComponentFactory : public Factory<IComponent, unsigned long>
+class ComponentFactory : public Factory<ASerializableComponent, unsigned long>
 {
+private:
+	void addComponent(ASerializableComponent *input)
+	{
+		this->add(Hash()(input->getType()), input);
+	}
+
 public:
 	ComponentFactory()
-	{ }
+	{}
 
 	virtual ~ComponentFactory(){
 	}
 
 	virtual void init()
 	{
-		//this->add(Hash()(new->getType()), input);
+		this->addComponent(new SFMLInputComponent());
+		this->addComponent(new Pos2DComponent());
+		this->addComponent(new Friction2DComponent());
+		this->addComponent(new Box2DComponent());
+		this->addComponent(new Speed2DComponent());
+		this->addComponent(new MovementSpeedComponent());
+		this->addComponent(new SFMLSpriteComponent());
+		this->addComponent(new NetworkSendUpdateComponent());
+		this->addComponent(new ActionComponent());
+		this->addComponent(new PlayerMovementComponent());
+		this->addComponent(new NetworkSendActionComponent());
 	}
 
-private:
-	std::string _directory;
+	virtual ASerializableComponent	*create(const unsigned long &key) const
+	{
+		const ASerializableComponent *tmp;
 
+		tmp = this->getOriginal(key);
+		if (!tmp)
+			return NULL;
+		return (tmp->cloneSerializable());
+	}
 };
 
-#endif /* !COMPONENTFACTORY_H_ */
+#endif
--- utility/ComponentFactory.hpp
+++ utility/ComponentFactory.hpp
@@ -19,43 +19,43 @@
 
 class ComponentFactory : public Factory<ASerializableComponent, unsigned long>
 {
-private :
-  void addComponent(ASerializableComponent *input)
-  {
-    this->add(Hash()(input->getType()), input);
-  }
+private:
+	void addComponent(ASerializableComponent *input)
+	{
+		this->add(Hash()(input->getType()), input);
+	}
 
-public :
-  ComponentFactory()
-  {}
+public:
+	ComponentFactory()
+	{}
 
-  virtual ~ComponentFactory(){
-  }
+	virtual ~ComponentFactory(){
+	}
 
-  virtual void init()
-  {
-    this->addComponent(new SFMLInputComponent());
-    this->addComponent(new Pos2DComponent());
-    this->addComponent(new Friction2DComponent());
-    this->addComponent(new Box2DComponent());
-    this->addComponent(new Speed2DComponent());
-    this->addComponent(new MovementSpeedComponent());
-    this->addComponent(new SFMLSpriteComponent());
-    this->addComponent(new NetworkSendUpdateComponent());
-    this->addComponent(new ActionComponent());
-    this->addComponent(new PlayerMovementComponent());
-    this->addComponent(new NetworkSendActionComponent());
-  }
+	virtual void init()
+	{
+		this->addComponent(new SFMLInputComponent());
+		this->addComponent(new Pos2DComponent());
+		this->addComponent(new Friction2DComponent());
+		this->addComponent(new Box2DComponent());
+		this->addComponent(new Speed2DComponent());
+		this->addComponent(new MovementSpeedComponent());
+		this->addComponent(new SFMLSpriteComponent());
+		this->addComponent(new NetworkSendUpdateComponent());
+		this->addComponent(new ActionComponent());
+		this->addComponent(new PlayerMovementComponent());
+		this->addComponent(new NetworkSendActionComponent());
+	}
 
-  virtual ASerializableComponent	*create(const unsigned long &key) const
-  {
-    const ASerializableComponent *tmp;
+	virtual ASerializableComponent	*create(const unsigned long &key) const
+	{
+		const ASerializableComponent *tmp;
 
-    tmp = this->getOriginal(key);
-    if (!tmp)
-      return NULL;
-    return (tmp->cloneSerializable());
-  }
+		tmp = this->getOriginal(key);
+		if (!tmp)
+			return NULL;
+		return (tmp->cloneSerializable());
+	}
 };
 
 #endif
