57b2eb21f7e4d2d9b3375e7c703048f889350edd
demess_p <demess_p@epitech.eu>
2014-05-30 18:05:07 +0200

    Modified AComponent so it already deserialize a bit before goind into specilization.

--- ECS/AComponent.hh
+++ ECS/AComponent.hh
@@ -1,12 +1,15 @@
 #ifndef ACOMPONENT_H_
 # define ACOMPONENT_H_
 
+# include	<algorithm>
 # include	<string>
+# include	<iostream>
+# include	<fstream>
+# include	<regex>
+
 # include	"IComponent.hh"
-
-
-# include <iostream>
-# include "Hash.hh"
+# include	"Hash.hh"
+#include	"EntityFileException.hh"
 
 class		AComponent : public IComponent
 {
@@ -29,14 +32,30 @@ public:
 
   virtual void		deserializeFromFile(std::ifstream &input)
   {
+    std::string		line;
+    while (std::getline(input, line))
+      {
+	line.erase(std::remove(line.begin(), line.end(), '\t'), line.end());
+
+	if (std::regex_match(line, std::regex("!COMPONENT")))
+	    return ;
+	else
+	  this->deserializeFromFileSpecial(line, input);
+      }
+  }
+
+  virtual void		deserializeFromFileSpecial(const std::string &lastline, std::ifstream &input)
+  {
+    (void)lastline;
     (void)input;
+    throw "deserializeFromFileSpecial() not defined for " + this->_type;
   }
 
   virtual void		serializeFromFile(std::ofstream &output, unsigned char indent) const
   {
     (void)output;
     (void)indent;
-    throw "Not defined for  " + this->_type;
+    throw "serializeFromFile not defined for " + this->_type;
   }
 };
 
