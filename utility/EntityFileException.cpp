55e094e4b7f431c35359078a42ee5b8564826d10
demess_p <demess_p@epitech.eu>
2014-05-31 00:34:08 +0200

    Fixed invalid read

--- utility/EntityFileException.cpp
+++ utility/EntityFileException.cpp
@@ -5,6 +5,7 @@
 EntityFileException::EntityFileException(std::string message, unsigned int lineno)
 {
   this->_message = message;
+  this->_component = "";
   this->_lineno = lineno;
   this->_filename = "Unkwown filename";
 }
@@ -19,20 +20,23 @@ EntityFileException::~EntityFileException()
 const char*	EntityFileException::what() const noexcept
 {
   std::stringstream	res;
+  std::string		*str = new std::string();
 
-  res << "===== EntityFileException =====" << std::endl;
+  // res << "===== EntityFileException =====" << std::endl;
   if (this->_filename != "")
     {
       res << "Where ?\tDuring parsing of [" << this->_filename << "]";
       if (this->_lineno > 0)
-	res << "(Line #" << this->_lineno << ")";
+      	res << "(Line #" << this->_lineno << ")";
       res << std::endl;
     }
   if (this->_component != "")
     res << "When ?\tWhen deserializing component " << this->_component << std::endl;
   res << "What ?\t" << this->_message << std::endl;
 
-  return (res.str().c_str());
+  *str = res.str();
+
+  return (str->c_str());
 }
 
 //----- ----- Setters ----- ----- //
