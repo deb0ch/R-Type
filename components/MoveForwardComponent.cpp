74102032d00dded7c6e3c5c622d0fcc6fba591c1
demess_p <demess_p@epitech.eu>
2014-05-30 12:09:03 +0200

    Updated the MoveForwardComponent so it takes 2 strings indicating the two direction to go (two for diagonals) instead of an enum. Updated the system as well.

--- components/MoveForwardComponent.cpp
+++ components/MoveForwardComponent.cpp
@@ -2,36 +2,11 @@
 #include "Pos2DComponent.hh"
 
 //----- ----- Constructors ----- ----- //
-MoveForwardComponent::MoveForwardComponent(MoveForwardComponent::Direction dir)
+MoveForwardComponent::MoveForwardComponent(const std::string &dir1, const std::string &dir2)
 : ACopyableComponent("MoveForwardComponent")
 {
-	this->_direction1 = dir;
-	this->_direction2 = NONE;
-	if (dir == MoveForwardComponent::UP)
-		this->_actions.push_back("UP");
-	else if (dir == MoveForwardComponent::DOWN)
-		this->_actions.push_back("DOWN");
-	else if (dir == MoveForwardComponent::RIGHT)
-		this->_actions.push_back("RIGHT");
-	else if (dir == MoveForwardComponent::LEFT)
-		this->_actions.push_back("LEFT");
-}
-
-
-MoveForwardComponent::MoveForwardComponent(MoveForwardComponent::Direction dir,
-	const Direction seconddir)
-	: ACopyableComponent("MoveForwardComponent")
-{
-	this->_direction1 = dir;
-	this->_direction2 = seconddir;
-	if (dir == MoveForwardComponent::UP || seconddir == MoveForwardComponent::UP)
-		this->_actions.push_back("UP");
-	if (dir == MoveForwardComponent::DOWN || seconddir == MoveForwardComponent::DOWN)
-		this->_actions.push_back("DOWN");
-	if (dir == MoveForwardComponent::RIGHT || seconddir == MoveForwardComponent::RIGHT)
-		this->_actions.push_back("RIGHT");
-	if (dir == MoveForwardComponent::LEFT || seconddir == MoveForwardComponent::LEFT)
-		this->_actions.push_back("LEFT");
+  this->_direction1 = dir1;
+  this->_direction2 = dir2;
 }
 
 //----- ----- Destructor ----- ----- //
@@ -40,37 +15,27 @@ MoveForwardComponent::~MoveForwardComponent()
 
 //----- ----- Methods ----- ----- //
 
-const std::vector<std::string> & MoveForwardComponent::getDirection() const
+const std::pair<std::string, std::string>	MoveForwardComponent::getDirection() const
 {
-	return (this->_actions);
+	return (std::make_pair(this->_direction1, this->_direction2));
 }
-
-// void MoveForwardComponent::setDirection(const MoveForwardComponent::Direction dir)
-// {
-// 	this->_direction = dir;
-// }
 
 void	MoveForwardComponent::serialize(IBuffer &buffer) const
 {
-  buffer << static_cast<char>(this->_direction1);
-  buffer << static_cast<char>(this->_direction2);
+  buffer << this->_direction1;
+  buffer << this->_direction2;
 }
 
 void		MoveForwardComponent::unserialize(IBuffer &buffer)
 {
-  char		tmp;
+  buffer >> this->_direction1;
+  buffer >> this->_direction2;
+}
 
-  buffer >> tmp;
-  this->_direction1 = static_cast<Direction>(tmp);
-  buffer >> tmp;
-  this->_direction2 = static_cast<Direction>(tmp);
-
-  if (this->_direction1 == MoveForwardComponent::UP || this->_direction2 == MoveForwardComponent::UP)
-    this->_actions.push_back("UP");
-  if (this->_direction1 == MoveForwardComponent::DOWN || this->_direction2 == MoveForwardComponent::DOWN)
-    this->_actions.push_back("DOWN");
-  if (this->_direction1 == MoveForwardComponent::RIGHT || this->_direction2 == MoveForwardComponent::RIGHT)
-    this->_actions.push_back("RIGHT");
-  if (this->_direction1 == MoveForwardComponent::LEFT || this->_direction2 == MoveForwardComponent::LEFT)
-    this->_actions.push_back("LEFT");
+void		MoveForwardComponent::serializeFromFile(std::ofstream &output, unsigned char indent) const
+{
+  if (this->_direction1 != "")
+    output << std::string(indent, '\t') << "dir1=" << this->_direction1 << std::endl;
+  if (this->_direction2 != "")
+    output << std::string(indent, '\t') << "dir2=" << this->_direction2 << std::endl;
 }
