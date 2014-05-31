bcc4f767f2464279b2ca493f201634a19e506c9d
ouabde_r <romain.ouabdelkader@gmail.com>
2014-05-31 20:09:41 +0200

    Removed SFML from SFMLSpriteComponent

--- components/SFMLSpriteComponent.cpp
+++ components/SFMLSpriteComponent.cpp
@@ -6,33 +6,193 @@
 SFMLSpriteComponent::SFMLSpriteComponent()
 : ACopyableComponent("SFMLSpriteComponent")
 {
-	this->_mapexist = false;
-	this->_currentSprite = 0;
-	this->_sprites.nbSprintX = 0;
-	this->_sprites.nbSprintY = 0;
-	this->_fileName = "";
-	this->_tickCounter = 0;
-	this->_counter = 0;
+  this->_currentSprite = 0;
+  this->_sprites.first = 0;
+  this->_sprites.second = 0;
+  this->_fileName = "";
+  this->_tickCounter = 0;
+  this->_counter = 0;
 }
 
-SFMLSpriteComponent::SFMLSpriteComponent(const std::string &filename, const ImageLoader::NbSprite& sprites,
-	const std::map<std::string, std::pair<int, int> > &map, unsigned int tickChange)
-	: ACopyableComponent("SFMLSpriteComponent")
+SFMLSpriteComponent::SFMLSpriteComponent(const std::string &filename, const std::pair<int, int>& sprites,
+					 const std::map<std::string, std::pair<int, int> > &map, unsigned int tickChange)
+  : ACopyableComponent("SFMLSpriteComponent")
 {
-	this->_fileName = filename;
-	this->_sprites = sprites;
-	this->_tickChange = tickChange;
-	this->_counter = 0;
-	this->_tickCounter = 0;
-	this->_currentSprite = 0;
-	this->_map = map;
-	this->_previousAction = "";
-	this->_mapexist = true;
+  this->_fileName = filename;
+  this->_sprites = sprites;
+  this->_tickChange = tickChange;
+  this->_counter = 0;
+  this->_tickCounter = 0;
+  this->_currentSprite = 0;
+  this->_map = map;
+  this->_previousAction = "";
 }
 
 //----- ----- Destructor ----- ----- //
 SFMLSpriteComponent::~SFMLSpriteComponent()
 {}
+
+bool		SFMLSpriteComponent::hasAction(const std::string & action)
+{
+  auto it = this->_map.find(action);
+  if (it == this->_map.end())
+    return (false);
+  return (true);
+}
+
+const std::string	&SFMLSpriteComponent::getFileName() const
+{
+  return this->_fileName;
+}
+
+const std::pair<int, int> &SFMLSpriteComponent::getSpriteDim() const
+{
+  return this->_sprites;
+}
+
+int			SFMLSpriteComponent::getCurrentSpriteNumber(const std::string &action)
+{
+  auto it = this->_map.find(action);
+
+  if (it == this->_map.end())
+    {
+      if (action == "")
+	return (-1);
+      it = this->_map.find("");
+      if (it == this->_map.end())
+	return (-1);
+    }
+  if (this->_previousAction == it->first)
+    ++this->_tickCounter;
+  else
+    {
+      this->_previousAction = it->first;
+      this->_tickCounter = 0;
+      this->_counter = 0;
+    }
+  if (this->_tickCounter > this->_tickChange)
+    {
+      ++(this->_counter);
+      if (this->_counter >= it->second.second)
+	this->_counter = 0;
+      this->_tickCounter = 0;
+    }
+  return (this->_counter + it->second.first);
+}
+
+void		SFMLSpriteComponent::serialize(IBuffer &buffer) const
+{
+  buffer << this->_fileName;
+  buffer << this->_sprites.first;
+  buffer << this->_sprites.second;
+  buffer << this->_tickChange;
+  buffer << static_cast<unsigned int>(this->_map.size());
+  std::for_each(this->_map.begin(), this->_map.end(),
+		[&buffer] (const std::pair< std::string, std::pair<int, int> > &pair)
+		{
+		  buffer << pair.first;
+		  buffer << pair.second.first;
+		  buffer << pair.second.second;
+		});
+}
+
+void		SFMLSpriteComponent::unserialize(IBuffer &buffer)
+{
+  unsigned int	nb_elem;
+  std::string	tmp;
+  std::pair<int, int> pair;
+
+  buffer >> this->_fileName;
+  buffer >> this->_sprites.first;
+  buffer >> this->_sprites.second;
+  buffer >> this->_tickChange;
+  buffer >> nb_elem;
+  this->_map.clear();
+  for(unsigned int i = 0; i < nb_elem; ++i)
+    {
+      buffer >> tmp;
+      buffer >> pair.first;
+      buffer >> pair.second;
+      this->_map[tmp] = pair;
+    }
+}
+
+void	SFMLSpriteComponent::deserializeFromFileSpecial(const std::string &lastline, std::ifstream &input,
+							unsigned int &lineno)
+{
+  if (std::regex_match(lastline, std::regex("filename=.+")))
+    this->_fileName = lastline.substr(9);
+  else if (std::regex_match(lastline, std::regex("nbSpritesX=.+")))
+    this->_sprites.first = std::stoi(lastline.substr(11));
+  else if (std::regex_match(lastline, std::regex("nbSpritesY=.+")))
+    this->_sprites.second = std::stoi(lastline.substr(11));
+  else if (std::regex_match(lastline, std::regex("tickChange=.+")))
+    this->_tickChange = std::stoi(lastline.substr(11));
+  else if (std::regex_match(lastline, std::regex("anim=ANIM")))
+    {
+      std::string		line;
+      std::string		action;
+      std::pair<int, int>	pair;
+      while (std::getline(input, line))
+	{
+	  ++lineno;
+	  line.erase(std::remove(line.begin(), line.end(), '\t'), line.end());
+	  if (std::regex_match(line, std::regex("action=.*")))
+	    action = line.substr(7);
+	  else if (std::regex_match(line, std::regex("start=.+")))
+	    pair.first = std::stoi(line.substr(6));
+	  else if (std::regex_match(line, std::regex("length=.+")))
+	    pair.second = std::stoi(line.substr(7));
+	  else if (std::regex_match(line, std::regex("!ANIM")))
+	    {
+	      this->_map[action] = pair;
+	      break ;
+	    }
+	  else
+	    throw EntityFileException("Bad argument for ANIM : \"" + line + "\"", lineno);
+	}
+    }
+  else
+    throw EntityFileException("Bad argument : \"" + lastline + "\"", lineno);
+}
+
+void		SFMLSpriteComponent::serializeFromFile(std::ofstream &output, unsigned char indent) const
+{
+  output << std::string(indent, '\t') << "filename=" << this->_fileName << std::endl;
+  output << std::string(indent, '\t') << "nbSpritesX=" << this->_sprites.first << std::endl;
+  output << std::string(indent, '\t') << "nbSpritesY=" << this->_sprites.second << std::endl;
+  output << std::string(indent, '\t') << "tickChange=" << this->_tickChange << std::endl;
+  std::for_each(this->_map.begin(), this->_map.end(), [&output, indent](const std::pair<std::string, std::pair<int, int> > &pair) {
+      output << std::string(indent, '\t') << "anim=ANIM" << std::endl;
+      output << std::string(indent + 1, '\t') << "action=" << pair.first << std::endl;
+      output << std::string(indent + 1, '\t') << "start=" << pair.second.first << std::endl;
+      output << std::string(indent + 1, '\t') << "length="<< pair.second.second << std::endl;
+      output << std::string(indent, '\t') << "!ANIM" << std::endl;
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 sf::Sprite	*SFMLSpriteComponent::getSpriteFromMap(ImageLoader &imageLoader, const std::string &action)
 {
@@ -92,107 +252,4 @@ sf::Sprite	*SFMLSpriteComponent::getSprite(ImageLoader &imageLoader, const std::
 	else
 		return (this->getSpriteWithoutMap(imageLoader, action));
 }
-
-bool		SFMLSpriteComponent::hasAction(const std::string & action)
-{
-	auto it = this->_map.find(action);
-	if (it == this->_map.end())
-		return (false);
-	return (true);
-}
-
-void		SFMLSpriteComponent::serialize(IBuffer &buffer) const
-{
-	int	first;
-	int	nb_sprites;
-	auto it = this->_map.find(this->_previousAction);
-
-	 // DANGEROUS (if you return here, unserialize cannot know it and will try to read more than it has)
-	// if (it == this->_map.end())
-	// 	return;
-
-	if (it != this->_map.end())
-	  {
-	    first = it->second.first;
-	    nb_sprites = it->second.second;
-	  }
-	else
-	  {
-	    first = 0;
-	    nb_sprites = 0;
-	  }
-	buffer << this->_fileName;
-	buffer << this->_sprites.nbSprintX;
-	buffer << this->_sprites.nbSprintY;
-	buffer << this->_tickChange;
-	buffer << this->_tickCounter;
-	buffer << this->_currentSprite;
-	buffer << first;
-	buffer << nb_sprites;
-}
-
-void		SFMLSpriteComponent::unserialize(IBuffer &buffer)
-{
-	buffer >> this->_fileName;
-	buffer >> this->_sprites.nbSprintX;
-	buffer >> this->_sprites.nbSprintY;
-	buffer >> this->_tickChange;
-	buffer >> this->_tickCounter;
-	buffer >> this->_currentSprite;
-	buffer >> this->_first;
-	buffer >> this->_nbSprite;
-}
-
-void	SFMLSpriteComponent::deserializeFromFileSpecial(const std::string &lastline, std::ifstream &input, unsigned int &lineno)
-{
-  this->_mapexist = true;
-  if (std::regex_match(lastline, std::regex("filename=.+")))
-    this->_fileName = lastline.substr(9);
-  else if (std::regex_match(lastline, std::regex("nbSpritesX=.+")))
-    this->_sprites.nbSprintX = std::stoi(lastline.substr(11));
-  else if (std::regex_match(lastline, std::regex("nbSpritesY=.+")))
-    this->_sprites.nbSprintY = std::stoi(lastline.substr(11));
-  else if (std::regex_match(lastline, std::regex("tickChange=.+")))
-    this->_tickChange = std::stoi(lastline.substr(11));
-  else if (std::regex_match(lastline, std::regex("anim=ANIM")))
-    {
-      std::string		line;
-      std::string		action;
-      std::pair<int, int>	pair;
-      while (std::getline(input, line))
-	{
-	  ++lineno;
-	  line.erase(std::remove(line.begin(), line.end(), '\t'), line.end());
-	  if (std::regex_match(line, std::regex("action=.*")))
-	    action = line.substr(7);
-	  else if (std::regex_match(line, std::regex("start=.+")))
-	    pair.first = std::stoi(line.substr(6));
-	  else if (std::regex_match(line, std::regex("length=.+")))
-	    pair.second = std::stoi(line.substr(7));
-	  else if (std::regex_match(line, std::regex("!ANIM")))
-	    {
-	      this->_map[action] = pair;
-	      break ;
-	    }
-	  else
-	    throw EntityFileException("Bad argument for ANIM : \"" + line + "\"", lineno);
-	}
-    }
-  else
-    throw EntityFileException("Bad argument : \"" + lastline + "\"", lineno);
-}
-
-void		SFMLSpriteComponent::serializeFromFile(std::ofstream &output, unsigned char indent) const
-{
-  output << std::string(indent, '\t') << "filename=" << this->_fileName << std::endl;
-  output << std::string(indent, '\t') << "nbSpritesX=" << this->_sprites.nbSprintX << std::endl;
-  output << std::string(indent, '\t') << "nbSpritesY=" << this->_sprites.nbSprintY << std::endl;
-  output << std::string(indent, '\t') << "tickChange=" << this->_tickChange << std::endl;
-  std::for_each(this->_map.begin(), this->_map.end(), [&output, indent](const std::pair<std::string, std::pair<int, int> > &pair) {
-      output << std::string(indent, '\t') << "anim=ANIM" << std::endl;
-      output << std::string(indent + 1, '\t') << "action=" << pair.first << std::endl;
-      output << std::string(indent + 1, '\t') << "start=" << pair.second.first << std::endl;
-      output << std::string(indent + 1, '\t') << "length="<< pair.second.second << std::endl;
-      output << std::string(indent, '\t') << "!ANIM" << std::endl;
-    });
-}
+*/
--- systems/SFMLRenderSystem.hh
+++ systems/SFMLRenderSystem.hh
@@ -2,6 +2,7 @@
 # define SFMLRENDERSYSTEM_H_
 
 # include		"SFML/Graphics.hpp"
+# include		"SFMLSpriteComponent.hh"
 # include		"Pos2DComponent.hh"
 # include		"Window.hh"
 # include		"ASystem.hh"
@@ -21,6 +22,8 @@ public:
   virtual void		start();
   virtual void		beforeProcess(const float);
   virtual void		afterProcess(const float);
+
+  sf::Sprite		*getSprite(ImageLoader &, SFMLSpriteComponent *component, const std::string &action);
 
   void			displayCollision(Entity *entity, sf::RenderWindow *);
   void			displayBox2D(float width, float height, Pos2DComponent *pos, sf::RenderWindow *);
