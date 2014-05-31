87d1f5e17c229e7fdb0af4ce49f94e957e336ecc
ouabde_r <romain.ouabdelkader@gmail.com>
2014-05-31 11:46:53 +0200

    Fix compilation (missing #ifdef for windows / linux)

--- utility/DirectoryLister/UDirectoryLister.cpp
+++ utility/DirectoryLister/UDirectoryLister.cpp
@@ -1,3 +1,5 @@
+#ifdef __linux__
+
 #include "UDirectoryLister.hh"
 
 DirectoryLister::DirectoryLister()
@@ -10,3 +12,5 @@ std::vector<std::string> DirectoryLister::listDirectory(const std::string &)
 {
   return std::vector<std::string>();
 }
+
+#endif
